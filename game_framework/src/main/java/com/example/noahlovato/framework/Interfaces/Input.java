3f23ba601e0897a1164cd6c5e8f0a6054d6c0c02
NoahLovato <noragami212>
2017-07-28 13:51:21 -0700

    Added TouchEvent class to input interface

--- game_framework/src/main/java/com/example/noahlovato/framework/Interfaces/Input.java
+++ game_framework/src/main/java/com/example/noahlovato/framework/Interfaces/Input.java
@@ -15,5 +15,15 @@ public interface Input {
         public char keyChar;
     }
 
+    public class TouchEvent {
+        public static final int TOUCH_DOWN = 0;
+        public static final int TOUCH_UP = 1;
+        public static final int TOUCH_DRAGGED = 2;
+
+        public int type;
+        public int x,y;
+        public int pointer;
+    }
+
 
 }
