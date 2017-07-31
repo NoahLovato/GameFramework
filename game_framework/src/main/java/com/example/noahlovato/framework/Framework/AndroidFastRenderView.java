8e475bfeb5584b5632d486d47fe79705798db790
NoahLovato <noragami212>
2017-07-31 13:31:12 -0700

    Finished RenderView

--- game_framework/src/main/java/com/example/noahlovato/framework/Framework/AndroidFastRenderView.java
+++ game_framework/src/main/java/com/example/noahlovato/framework/Framework/AndroidFastRenderView.java
@@ -1,5 +1,9 @@
 package com.example.noahlovato.framework.Framework;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 /**
@@ -7,4 +11,61 @@ import android.view.SurfaceView;
  */
 
 public class AndroidFastRenderView extends SurfaceView implements Runnable {
+
+    AndroidGame game;
+    Bitmap frameBuffer;
+    Thread renderThread = null;
+    SurfaceHolder holder;
+    volatile boolean running = false;
+
+    public AndroidFastRenderView(AndroidGame game, Bitmap frameBuffer){
+        super(game);
+        this.game = game;
+        this.frameBuffer = frameBuffer;
+        this.holder = getHolder();
+    }
+
+    public void resume() {
+        running = true;
+        renderThread = new Thread(this);
+        renderThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        Rect dstRect = new Rect();
+        long startTime = System.nanoTime();
+
+        while(running){
+
+            if(!holder.getSurface().isValid())
+                continue;
+
+            float deltaTime = (System.nanoTime() - startTime) / 1000000000.0f;
+            startTime = System.nanoTime();
+
+            game.getCurrentScreen().update(deltaTime);
+            game.getCurrentScreen().present(deltaTime);
+
+            Canvas canvas = holder.lockCanvas();
+            canvas.getClipBounds(dstRect);
+            canvas.drawBitmap(frameBuffer, null, dstRect, null);
+            holder.unlockCanvasAndPost(canvas);
+
+        }
+    }
+
+    public void pause(){
+        running = false;
+        while (true){
+            try {
+                renderThread.join();
+                return;
+            }
+            catch (InterruptedException e){
+
+            }
+        }
+    }
 }
---
+++ game_framework/src/main/java/com/example/noahlovato/framework/Framework/AndroidGame.java
@@ -0,0 +1,12 @@
+package com.example.noahlovato.framework.Framework;
+
+import android.app.Activity;
+
+import com.example.noahlovato.framework.Interfaces.Game;
+
+/**
+ * Created by noah.lovato on 7/31/2017.
+ */
+
+public class AndroidGame extends Activity implements Game {
+}
